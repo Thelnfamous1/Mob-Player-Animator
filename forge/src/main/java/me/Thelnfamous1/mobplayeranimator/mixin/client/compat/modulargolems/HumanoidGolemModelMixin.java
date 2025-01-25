44ffd10ab53d3f82ff83ddcfc6496cb2822d71d4
Thelnfamous1 <junior.garay1999@gmail.com>
2025-01-25 02:10:19 -0600

    Fix #14. Update mod version to 1.3.3.

---
+++ forge/src/main/java/me/Thelnfamous1/mobplayeranimator/mixin/client/compat/modulargolems/HumanoidGolemModelMixin.java
@@ -0,0 +1,21 @@
+package me.Thelnfamous1.mobplayeranimator.mixin.client.compat.modulargolems;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.xkmc.modulargolems.content.entity.humanoid.HumanoidGolemEntity;
+import dev.xkmc.modulargolems.content.entity.humanoid.HumanoidGolemModel;
+import me.Thelnfamous1.mobplayeranimator.api.PlayerAnimatorHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(HumanoidGolemModel.class)
+public class HumanoidGolemModelMixin {
+
+    @WrapOperation(method = "setupAnim(Ldev/xkmc/modulargolems/content/entity/humanoid/HumanoidGolemEntity;FFFFF)V", at = @At(value = "INVOKE", target = "Ldev/xkmc/modulargolems/content/entity/humanoid/HumanoidGolemEntity;isAggressive()Z"))
+    private boolean wrap_isAggressive_setupAnim(HumanoidGolemEntity instance, Operation<Boolean> original){
+        if(PlayerAnimatorHelper.isAnimating(instance)){
+            return false;
+        }
+        return original.call(instance);
+    }
+}
